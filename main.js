f1e913674351d811038dd70ee7dfd9bbf3c22bbb
Patrick Thurston <patsdroid18@gmail.com>
2019-02-14 20:11:43 -0700

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+/*
+1. The goal is to collect the circumference of a sphere at the equator and calculate how many rotations per minute it would take
+for an object on its surface to reach light speed.
+2. Secondary goal is to add the ability to call this from an api and return a varity of different answers depending on input.
+*/
+
+let circumference = document.getElementById("circ-input").value;
