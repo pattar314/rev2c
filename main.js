767c75fa86a36a87934415f0d686f68b2264bf1e
Patrick Thurston <patsdroid18@gmail.com>
2019-02-23 18:42:46 -0700

    test

--- main.js
+++ main.js
@@ -4,4 +4,13 @@ for an object on its surface to reach light speed.
 2. Secondary goal is to add the ability to call this from an api and return a varity of different answers depending on input.
 */
 
-let circumference = document.getElementById("circ-input").value;
+let in_form = document.getElementById("#circ-form");
+in_form.addEventListener("submit", event => {
+    alert("It works");
+    let size = in_form.elements.circ_in.value;
+    event.preventDefault();
+    let c = 299792458;
+    let rpm_needed = (c / size);
+    let solution = document.getElementById("#results");
+    solution.elements = "It worked";
+});
