7a1953369a8356525a649671f2a11a7a47a42459
Patrick Thurston <patsdroid18@gmail.com>
2019-02-23 19:34:26 -0700

    First draft of rev2c_main.py

--- rev2c_main.py
+++ rev2c_main.py
@@ -1,1 +1,4 @@
-
+size = int(input("What is the circumference of the sphere at the equater in meteres: "))
+c = 299792458;
+rpm_needed = round((c / size), 3);
+return(f"\n\nThe sphere would have to rotate at {rpm_needed}rpm for an object on its surface to reach lightspeed.")
