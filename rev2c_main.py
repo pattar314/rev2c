38ec00014195036dbb12ef71db0f1240bc560194
Patrick Thurston <patsdroid18@gmail.com>
2019-02-23 20:10:19 -0700

    polished rev2c_main.py added ability to pass in argument from command line and fixed math errors

--- rev2c_main.py
+++ rev2c_main.py
@@ -1,4 +1,16 @@
-size = int(input("What is the circumference of the sphere at the equater in meteres: "))
-c = 299792458;
-rpm_needed = round((c / size), 3);
-return(f"\n\nThe sphere would have to rotate at {rpm_needed}rpm for an object on its surface to reach lightspeed.")
+import sys
+
+
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1].isdigit():
+            size = int(sys.argv[1])
+    else:
+        size = int(input("What is the circumference of the sphere at the equater in meteres: "))
+        
+    c = 299792458
+    rpm_needed = round((c / size) * 60, 3)
+    print(f"\n\nThe sphere would have to rotate at {rpm_needed} rpm for an object on its surface to reach lightspeed.")
+
+main()
